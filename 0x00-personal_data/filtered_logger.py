d125df85cb0f3eed0312bcf87ce2ef22d71f19c9
Faith-qa <atienofaith12@gmail.com>
2021-08-20 05:12:36 +0300

    changes

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""obfuscates a log message"""
+"""Obfuscates a log message"""
 
 from typing import List
 import re
 import logging
-# import mysql.connector
+ # import mysql.connector
 from os import getenv
 
 
@@ -12,11 +12,25 @@ PII_FIELDS = ('name', 'password', 'phone', 'ssn', 'email')
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
-                 seperator: str) -> str:
-    """returns a log message obfucated"""
+                 separator: str) -> str:
+    """Returns a log message obfuscated"""
 
     for i in fields:
-        message = re.sub(i + "*?" + seperator,
-                         i + "=" + redaction + seperator, message)
-
+        message = re.sub(i + "=.*?" + separator,
+                         i + "=" + redaction + separator,
+                         message)
     return message
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
