4077b15c116bfb9e2243bd2b0e7a5977ab988baf
Faith-qa <atienofaith12@gmail.com>
2021-08-20 04:35:00 +0300

    changes

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,6 +8,9 @@ import logging
 from os import getenv
 
 
+PII_FIELDS = ('name', 'password', 'phone', 'ssn', 'email')
+
+
 def filter_datum(fields: List[str], redaction: str, message: str,
                  seperator: str) -> str:
     """returns a log message obfucated"""
